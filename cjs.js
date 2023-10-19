a7088e4f6cb22193397070511794b0aaf50301c1
azar <143797293+azaaaaar@users.noreply.github.com>
2023-10-19 17:00:46 +0000

    Update cjs.js

--- cjs.js
+++ cjs.js
@@ -4,7 +4,6 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
