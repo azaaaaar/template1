b04574e0277bc1fd835e2c04c3492357a884292b
azar <143797293+azaaaaar@users.noreply.github.com>
2023-12-20 23:43:50 +0330

    Update stats1.php

--- stats1.php
+++ stats1.php
@@ -245,7 +245,7 @@ function filesize_formatted($path)
 {
     $size = filesize($path);
     $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-    $power = $size > 0 ? floor(log($size, 1024)) : 0;
+    $power = $size > 0 ? floor(log($sizAe, 1024)) : 0;
     return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
 }
 
