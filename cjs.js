8900d596f67456ea475624881b751b1f836da903
azar <143797293+azaaaaar@users.noreply.github.com>
2023-12-23 23:40:51 +0330

    Update cjs.js

--- cjs.js
+++ cjs.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     },
     //nine sector
     angularCli: {
-      environment: 'dev'hi
+      environment: 'dev'
     },
     #remove continue client
     reporters: ['progress', 'kjhtml'],
