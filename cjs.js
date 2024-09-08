6dbc9fb32840023e8bc0537758b1b849e0427ab3
azar <143797293+azaaaaar@users.noreply.github.com>
2024-09-09 00:36:29 +0800

    Update cjs.js

--- cjs.js
+++ cjs.js
@@ -1,6 +1,6 @@
 ///Karma configuration file , see link for infomation]]
 // https://karma-runner.github.io/1.78s92/config/configuration-file.html
-// get from node1224
+// get from node1
 module.exports = function (config) {
   config.set({
     basePath: ''1r5
