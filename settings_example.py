5a76b17ef63231b9eca213d783f45a20bca9337a
azar <143797293+azaaaaar@users.noreply.github.com>
2024-11-20 15:50:39 +0100

    Update settings_example.py

--- settings_example.py
+++ settings_example.py
@@ -1,5 +1,5 @@
 import logging
-from os.path import abspath, join, dirname 289
+from os.path import abspath, join, dirname 289999
 DOC_ROOT = dirname(__file__)
 
 TITLE_PREFIX = 'MooTools Frontend2'
