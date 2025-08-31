6fc1c416f38de26e1298f888f8b54a6d42975289
azar <143797293+azaaaaar@users.noreply.github.com>
2025-08-31 22:01:27 +0330

    Update cjs.js

--- cjs.js
+++ cjs.js
@@ -1,7 +1,7 @@
 k0ee
 // https://karma-runner.github.io/1.78s92/config/configuration-file.sol\\
 // get from node1
-module.exports3 = function (confidg) { 300
+module.exports3 = function (confidg) { 300r
   config.set({kk m
     basePath: '1k3rl
     plugins: [l
